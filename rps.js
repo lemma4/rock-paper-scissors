82885a55f906a8c5eb8aeb0093741a0eaca32535
Subhan Eminzada <191730525+lemma4@users.noreply.github.com>
2025-02-23 20:07:04 -0500

    Finalize RPS by adding while loop to have the game go up to 5 rounds

--- rps.js
+++ rps.js
@@ -26,34 +26,56 @@ function getHumanChoice() {
 }
 }
 
+
+
+
+
+function playGame() {
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie! Let's try again!");
-    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-        computerScore += 1;
-        console.log('You lose! Paper beats rock.');
-    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        humanScore += 1;
-        console.log('You win! Rock beats scissors.');
-    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        humanScore += 1;
-        console.log('You win! Paper beats rock.');
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        computerScore += 1;
-        console.log('You lose! Scissors beat paper.');
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        computerScore += 1;
-        console.log('You lose! Rock beats scissors.');
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        humanScore += 1;
-        console.log('You win! Scissors beat paper.');
-    } else {
-        console.log('Something went wrong.');
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie! Let's try again!");
+        } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+            computerScore += 1;
+            console.log('You lose! Paper beats rock.');
+        } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            humanScore += 1;
+            console.log('You win! Rock beats scissors.');
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore += 1;
+            console.log('You win! Paper beats rock.');
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            computerScore += 1;
+            console.log('You lose! Scissors beat paper.');
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore += 1;
+            console.log('You lose! Rock beats scissors.');
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            humanScore += 1;
+            console.log('You win! Scissors beat paper.');
+        }
+    }
+
+
+
+
+while (true) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+    
+    console.log(`You ${humanScore} - ${computerScore} Computer`)
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore === 5) {
+            console.log('Congratulations! You win!')
+        } else if (computerScore === 5) {
+            console.log('Oh no, You lost! Better luck next time.')
+        }
+        break;
     }
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+}
+playGame();
